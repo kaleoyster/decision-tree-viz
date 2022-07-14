33414ccfb61959c765556b803d5a55448e618e88
Akshay Kale <akshaykale915@gmail.com>
2022-07-14 18:28:51 -0500

    update: created logs and saved files

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -9,9 +9,12 @@ Date:
 import sys
 import pandas as pd
 import numpy as np
+from six import StringIO
 from sklearn.datasets import load_iris
 from sklearn import tree
 import graphviz
+from sklearn.tree import export_graphviz
+import pydotplus
 #import matplotlib as plt
 
 def load_dataset():
@@ -25,6 +28,27 @@ def load_dataset():
     """
     iris = load_iris()
     return iris
+
+def save_decision_tree(clf, features, dataset_class_names, filename):
+    """
+    Description:
+        Save a png of the created decision tree
+    Args:
+        None
+    Returns:
+        an exported decision tree
+    """
+    # filename
+
+    # Save the decision tree
+    dot_data = StringIO()
+    export_graphviz(clf, out_file=dot_data,
+                    filled=True, rounded=True,
+                    special_characters=True,
+                    feature_names = features,
+                    class_names=['0','1', '2'])
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+    graph.write_png(filename)
 
 def print_decision_paths(clf, X_test, features):
     """
@@ -63,7 +87,7 @@ def print_decision_paths(clf, X_test, features):
                 node_indicator.indptr[sample_id] : node_indicator.indptr[sample_id + 1]
             ]
 
-            print("Rules used to predict sample {id}:\n".format(id=sample_id))
+            #print("Rules used to predict sample {id}:\n".format(id=sample_id))
             for node_id in node_index:
                 # Continue to the next node if it is a leaf node
                 if leaf_id[sample_id] == node_id:
@@ -81,23 +105,24 @@ def print_decision_paths(clf, X_test, features):
 
                 else:
                     threshold_sign = ">"
-                    print(
-                    "decision node {node} : (X_test[{sample}, {feature}] = {value}) "
-                    "{inequality} {threshold})".format(
-                        node=node_id,
-                        sample=sample_id,
-                        feature=feature[node_id],
-                        value=X_test[sample_id, feature[node_id]],
-                        inequality=threshold_sign,
-                        threshold=threshold[node_id],
-                    )
-                )
+            #        print(
+            #        "decision node {node} : (X_test[{sample}, {feature}] = {value}) "
+            #        "{inequality} {threshold})".format(
+            #            node=node_id,
+            #            sample=sample_id,
+            #            feature=feature[node_id],
+            #            value=X_test[sample_id, feature[node_id]],
+            #            inequality=threshold_sign,
+            #            threshold=threshold[node_id],
+            #        )
+            #    )
                 nodeList.append(node_id)
                 sampleIdList.append(sample_id)
                 featureIdList.append(features[feature[node_id]])
                 valueList.append(X_test[sample_id, feature[node_id]])
                 inequalityList.append(threshold_sign)
                 thresholdList.append(threshold[node_id])
+
     #sys.stdout = oStdout
     data = pd.DataFrame({
                      'node': nodeList,
@@ -107,21 +132,30 @@ def print_decision_paths(clf, X_test, features):
                      'inequality': inequalityList,
                      'threshold': thresholdList
                     })
-
     data.to_csv("path.csv")
+
+def log_decision_tree(clf, feature_cols, filename):
+    # Export to text (decision tree)
+    r = tree.export_text(clf, feature_names=feature_cols)
+    with open(filename, 'w') as fout:
+        fout.write(r)
 
 
 def main():
     """
     Driver function
     """
-
+    # Decision Tree
     iris = load_dataset()
+    file_log = 'iris-decision-tree.log'
+    filename = 'iris-decision-tree.png'
 
     # Splitting the dataset
     removed =[0, 10, 20, 30, 50, 60, 70, 80, 90, 100]
     new_target = np.delete(iris.target, removed)
-    new_data = np.delete(iris.data, removed, axis=0)
+    new_data = np.delete(iris.data,
+                         removed,
+                         axis=0)
     features = iris['feature_names']
     X_test = iris.data[removed]
 
@@ -134,27 +168,22 @@ def main():
     # Train data on new data and new target
     tree.plot_tree(clf)
 
+    feature_cols = iris['feature_names']
+    filename_log = 'decision-tree.log'
+    log_decision_tree(clf, features, file_log)
+
     # Make predictions
     prediction = clf.predict(iris.data[removed])
     dot_data = tree.export_graphviz(clf, out_file=None)
     iris = load_iris()
 
-    # Export
-    r = tree.export_text(clf, feature_names=iris['feature_names'])
-    tree.export_graphviz(clf, out_file='tree.dot')
-    dot_data = tree.export_graphviz(clf, out_file=None,
-                     feature_names=iris.feature_names,
-                     class_names=iris.target_names,
-                     filled=True, rounded=True,
-                     special_characters=True)
-    graph = graphviz.Source(dot_data)
+    # Assign removed data as input -> predictions
+    print("Original Labels", iris.target[removed])
+    print("Labels Predicted", prediction)
 
-    # Assign removed data as input
-    #print("Original Labels", iris.target[removed])
-    #print("Labels Predicted", prediction)
-
+    # Export a data frame of rules
     print_decision_paths(clf, X_test, features)
-
+    save_decision_tree(clf, features, iris.target_names, filename)
 
 if __name__=="__main__":
     main()
