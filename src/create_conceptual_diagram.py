d21db423e02fa532d7bfab41038cadece98fdb39
Akshay Kale <akshaykale915@gmail.com>
2022-10-05 23:06:33 -0500

    update: added new files and made changes to create_conceptual_diagram

--- src/create_conceptual_diagram.py
+++ src/create_conceptual_diagram.py
@@ -2,7 +2,10 @@
 Description:
     Create conceptual diagram with respect to IRIS dataset
 """
+import sys
 import csv
+import random
+import argparse
 import pandas as pd
 from concepts import Context
 from collections import defaultdict
@@ -16,6 +19,19 @@ def read_file(path):
         paths = csv.reader(csv_file, delimiter=',')
         header = next(paths)
         for row in paths:
+            all_paths.append(row)
+    return all_paths
+
+def read_sample(sample):
+    """
+    Returns a list of decision paths
+    """
+    all_paths = []
+    with open(sample, 'r') as csv_file:
+        paths = csv.reader(csv_file, delimiter=',')
+        header = next(paths)
+        for row in paths:
+            row = row[0]
             all_paths.append(row)
     return all_paths
 
@@ -101,162 +117,18 @@ def main():
     """
     Driver function
     """
-    path = 'path.csv'
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-i', '--inputfile', help='input filename')
+    parser.add_argument('-s', '--sample', help='sample filename')
+
+    args = parser.parse_args()
+    path = args.inputfile
+    sample_file = args.sample
+
     paths = read_file(path)
-
-    # class 0
-    filter_sample_0 = ['50',
-                     '51',
-                     '52',
-                     '53',
-                     '54',
-                     '55',
-                     '56',
-                     '58',
-                     '59',
-                     '61',
-                     '63',
-                     '64',
-                     '65',
-                     '66',
-                     '68',
-                     '70',
-                     '71',
-                     '72',
-                     '73',
-                     '74',
-                     '75',
-                     '76',
-                     '77',
-                     '78',
-                     '82',
-                     '83',
-                     '84',
-                     '85',
-                     '86',
-                     '87',
-                     '88',
-                     '89',
-                     '90',
-                     '91',
-                     '92',
-                     '94',
-                     '95',
-                     '96',
-                     '97',
-                     '99',
-                     '100',
-                     '101',
-                     '102',
-                     '103',
-                     '104',
-                     '105',
-                     '106',
-                     '107',
-                     '108',
-                     '109',
-                     '110',
-                     '111',
-                     '112',
-                     '113',
-                     '114',
-                     '115',
-                     '116',
-                     '117',
-                     '118',
-                     '119',
-                     '120',
-                     '121',
-                     '122',
-                     '123',
-                     '124',
-                     '125',
-                     '126',
-                     '127',
-                     '128',
-                     '129',
-                     '130',
-                     '131',
-                     '132',
-                     '133',
-                     '134',
-                     '135',
-                     '136',
-                     '137',
-                     '138',
-                     '139',
-                     '140',
-                     '141',
-                     '142',
-                     '143',
-                     '144',
-                     '145',
-                     '146',
-                     '147',
-                     '148',
-                     '149']
-    # Class 1:
-    filter_sample_1 = ['0',
-                        '1',
-                        '2',
-                        '3',
-                        '4',
-                        '5',
-                        '6',
-                        '7',
-                        '8',
-                        '9',
-                        '10',
-                        '11',
-                        '12',
-                        '13',
-                        '14',
-                        '15',
-                        '16',
-                        '17',
-                        '18',
-                        '19',
-                        '20',
-                        '21',
-                        '22',
-                        '23',
-                        '24',
-                        '25',
-                        '26',
-                        '27',
-                        '28',
-                        '29',
-                        '30',
-                        '31',
-                        '32',
-                        '33',
-                        '34',
-                        '35',
-                        '36',
-                        '37',
-                        '38',
-                        '39',
-                        '40',
-                        '41',
-                        '42',
-                        '43',
-                        '44',
-                        '45',
-                        '46',
-                        '47',
-                        '48',
-                        '49',
-                        '57',
-                        '60',
-                        '62',
-                        '67',
-                        '69',
-                        '79',
-                        '80',
-                        '81',
-                        '93',
-                        '98']
-
-    create_conceptual_table(paths, filter_sample_0)
+    samples = read_sample(sample_file)
+    samples = random.sample(samples, 50)
+    create_conceptual_table(paths, samples)
 
 main()
---
+++ src/create_sample.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def create_sample(file):
+    df = pd.read_csv(file)
+    sample_1 = df['structureNumber']
+    return sample_1
+
+def main():
+    path = 'nebraska_paved.csv'
+    samples = create_sample(path)
+    samples.to_csv('samples.csv', index=False)
+
+main()
